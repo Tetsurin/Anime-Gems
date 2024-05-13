142ced6499168b5fe3c93cf9d791124fd265d910
Tetsurin <v.tetchenko@gmail.com>
2024-05-13 16:15:18 +0300

    add random anime functionality

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const randomAnimeLink = document.getElementById('random-anime-link');
+    randomAnimeLink.addEventListener('click', () => {
+      const randomIndex = Math.floor(Math.random() * <%= Anime.count %>) + 1;
+      localStorage.setItem('randomAnimeIndex', randomIndex);
+    });
+  
+   
+    const savedRandomIndex = localStorage.getItem('randomAnimeIndex');
+    if (savedRandomIndex !== null) {
+      localStorage.removeItem('randomAnimeIndex'); 
+      const randomUrl = `/animes/${savedRandomIndex}`;
+      window.location.href = randomUrl;
+    }
+  });
